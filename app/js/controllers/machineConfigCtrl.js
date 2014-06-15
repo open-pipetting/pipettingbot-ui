6c1eac8c9979dc197fb54dce6ff3451b9007b1ff
Ciro S. Costa <ciro.costa@usp.br>
2014-06-15 15:27:27 -0300

    updates machine config - start positionShower implementation

--- app/js/controllers/machineConfigCtrl.js
+++ app/js/controllers/machineConfigCtrl.js
@@ -2,5 +2,20 @@ angular.module('pbUi.machineConfigCtrl', [])
   .controller('machineConfigCtrl', ['$scope', function($scope) {
 
   	$scope.showHelp = false;
+  	$scope.isCalibrating = false;
+
+  	function _keyListener (e) {
+  		console.log(e);
+  	}
+
+  	$scope.toggleCalibration = function () {
+  		$scope.isCalibrating = !$scope.isCalibrating;
+
+  		if ($scope.isCalibrating) {
+	  		$('body').bind('keypress', _keyListener);
+  		} else {
+				$('body').unbind('keypress', _keyListener);
+  		}
+  	};
 
   }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,6 +3,31 @@
 /* Directives */
 
 angular.module('pbUi.directives', [])
+  .directive('pbPositionshower', [function () {
+    function startWebGL (canvasElem) {
+      var gl = canvasElem.getContext("webgl") ||
+               canvasElem.getContext("experimental-webgl");
+
+      if (gl) {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+      }
+
+      return gl;
+    }
+
+    return {
+      restrict: 'E',
+      template: '<canvas width="578" height="200"></canvas>',
+      link: function (scope, elems, attrs) {
+        var canvas = elems[0].children[0];
+        startWebGL(canvas);
+
+      }
+    };
+  }])
   .directive('pbDragFile', ['$q', function ($q) {
     var $fileChooser = jQuery('#file-dialog')
         , Workbook = {}
