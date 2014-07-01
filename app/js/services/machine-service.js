8eba02fd2b193d5dc807a04b1225e9f62c455c0b
Ciro S. Costa <ciro.costa@usp.br>
2014-07-01 11:03:28 -0300

    adds true persistance

--- app/js/services/machine-service.js
+++ app/js/services/machine-service.js
@@ -1,19 +1,22 @@
 angular.module('pbUi.machineService', [])
   .factory('MachineService', [function () {
+    var defaultParams = [
+      {
+        name: 'Altura Total',
+        token: 'total-height',
+        value: 2000
+      },
+      {
+        name: 'Largura Total',
+        token: 'total-width',
+        value: 1000
+      }
+    ];
+
+    var model = angular.fromJson(localStorage.machineParams) || defaultParams;
 
     var service = {
-        model: [
-        {
-          name: 'Altura Total',
-          token: 'total-height',
-          value: 2000
-        },
-        {
-          name: 'Largura Total',
-          token: 'total-width',
-          value: 1000
-        }
-      ],
+      model: model,
 
       SaveState: function () {
         localStorage.machineParams = angular.toJson(service.model);
